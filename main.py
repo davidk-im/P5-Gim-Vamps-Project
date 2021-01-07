6b441c47fd659bc68d9e94848d8396f655bf2ebc
avabrooks <72889462+avabrooks@users.noreply.github.com>
2021-01-07 14:12:50 -0800

    delete about page

--- main.py
+++ main.py
@@ -17,9 +17,6 @@ def play():
 def signup():
     return render_template("signup.html")
 
-@app.route('/about')
-def aboutus():
-    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(port='3000', host='127.0.0.1')
