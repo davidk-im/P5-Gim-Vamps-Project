b11a2ef7d564bc6f842e43fbd800ecb21b881c98
notkobalt <lucas.bruner18@gmail.com>
2021-01-07 02:22:49 -0800

    Added about page, fixed background

--- main.py
+++ main.py
@@ -17,5 +17,9 @@ def play():
 def signup():
     return render_template("signup.html")
 
+@app.route('/about')
+def aboutus():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(port='3000', host='127.0.0.1')
