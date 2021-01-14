468be2eb9adf462128646d7987d277d4756691aa
avabrooks <72889462+avabrooks@users.noreply.github.com>
2021-01-14 14:02:50 -0800

    flaskmain.py

---
+++ flaskmain.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+#home page route
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/play')
+def playmenu():
+    return render_template("playmenu.html")
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+
+if __name__ == "__main__":
+    app.run(port='3000', host='127.0.0.1')
+
--- views.py
+++
@@ -1,1 +0,0 @@
-#for potential use of a landing site in the future: sign in/sign up
