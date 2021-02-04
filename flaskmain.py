c31aca875282260bfe5eee102fd8d78ce198aa69
avabrooks <72889462+avabrooks@users.noreply.github.com>
2021-02-03 23:15:19 -0800

    login page with users(not connected to db yet)

--- flaskmain.py
+++ flaskmain.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, g, url_for
+from flask import (Flask, g, redirect, render_template, request, session, url_for)
 from user import user_create
 from flask_sqlalchemy import SQLAlchemy
 import requests
@@ -18,7 +18,61 @@ db = SQLAlchemy(app)
 db.init_app(app)
 
 
+##login trial
+class User:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
 
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+users = []
+users.append(User(id=1, username='avabrooks', password='password'))
+users.append(User(id=2, username='lucasbruner', password='secret'))
+users.append(User(id=3, username='davidkim', password='okay'))
+users.append(User(id=4, username='kylemyint', password='chess'))
+
+
+app = Flask(__name__)
+app.secret_key = '12secure34'
+
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'user_id' in session:
+        user = [x for x in users if x.id == session['user_id']][0]
+        g.user = user
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+
+        username = request.form['username']
+        password = request.form['password']
+
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('profile'))
+
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+
+@app.route('/profile')
+def profile():
+    if not g.user:
+        return redirect(url_for('login'))
+
+    return render_template('profile.html')
+########################################################################################################################
+########################################################################################################################
 #home page route
 @app.route('/')
 def home():
@@ -63,10 +117,10 @@ def leaderboards():
 def logresults():
     return render_template("logresults.html")
 
-@app.route('/profile')
-@login_required
-def profile():
-    return render_template("profile.html")
+#@app.route('/profile')
+#@login_required
+#def profile():
+ #   return render_template("profile.html")
 
 @app.route('/lichesslb', methods=['GET', 'POST'])
 def lichesslb():
@@ -82,7 +136,8 @@ def lichesslb():
 
 
 
-
+########################################################################################################################
+########################################################################################################################
 #Chess code
 @app.route("/createBoardTable", methods=['GET','POST']) #this is is where the website directs to when clicking the submit button
 def createBoardTable():
